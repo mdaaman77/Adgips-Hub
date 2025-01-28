e3b5f69b78dc6019054e98f0bb35758e07931edc
mdaaman77 <mdaaman31241@gmail.com>
2025-01-28 21:21:20 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="bg-black/90 border-t border-gray-800">
       <div className="max-w-7xl mx-auto px-4 py-6">
         <div className="text-center text-gray-400">
-          <p>© {new Date().getFullYear()} Ece Boys . All rights reserved </p>
+          <p>© {new Date().getFullYear()}  Boys . All rights reserved </p>
           <p className="mt-2 text-sm">
             Created with ❤️
           </p>
